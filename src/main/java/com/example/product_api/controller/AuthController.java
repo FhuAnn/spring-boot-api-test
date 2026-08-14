984c8f9642f7822f9a5b2a9261dc3e041c803e60
FhuAnn <phuandongthap02@gmail.com>
2026-08-14 17:48:02 +0700

    add standard structure code

--- src/main/java/com/example/product_api/controller/AuthController.java
+++ src/main/java/com/example/product_api/controller/AuthController.java
@@ -1,8 +1,107 @@
 package com.example.product_api.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.product_api.dto.ProfileDTO;
+import com.example.product_api.io.AuthRequest;
+import com.example.product_api.io.AuthResponse;
+import com.example.product_api.io.ProfileRequest;
+import com.example.product_api.io.ProfileResponse;
+import com.example.product_api.service.CustomerUserDetailsService;
+import com.example.product_api.service.ProfileService;
+import com.example.product_api.service.TokenBlacklistService;
+import com.example.product_api.util.JwtTokenUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @RestController
+@Slf4j
+@RequiredArgsConstructor
 public class AuthController {
-    
+    private final ModelMapper modelMapper;
+    private final AuthenticationManager authenticationManager;
+    private final TokenBlacklistService tokenBlacklistService;
+    private final ProfileService profileService;
+    private final CustomerUserDetailsService userDetailsService;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/register")
+    public ProfileResponse createProfile(@Valid @RequestBody ProfileRequest request) {
+        log.info("API /register is called {}", request);
+        ProfileDTO profileDTO = mapToProfileDTO(request);
+        profileDTO = profileService.createProfile(profileDTO);
+        log.info("Printing the profile dto details {}", profileDTO);
+        return mapToProfileResponse(profileDTO);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse authenticateProfile(@RequestBody AuthRequest request) throws Exception {
+        log.info("API /login is called {}", request);
+        authenticate(request);
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+        final String jwtToken = jwtTokenUtil.generateToken(userDetails);
+        return new AuthResponse(jwtToken, request.getEmail());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping("/signout")
+    public void signout(HttpServletRequest request) {
+        String jwtToken = extractJwtTokenFromRequest(request);
+        if (jwtToken != null) {
+            tokenBlacklistService.addTokenToBlacklist(jwtToken);
+        }
+    }
+
+    private String extractJwtTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    private void authenticate(AuthRequest request) throws Exception {
+        try {
+            authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        } catch (DisabledException ex) {
+            throw new Exception("Profile disabled");
+        } catch (Exception ex) {
+            throw new Exception("Invalid credentials");
+        }
+    }
+
+    /**
+     * Mapper method to map values from profile request to profile dto
+     * 
+     * @param profileRequest
+     * @return profileDto
+     */
+    private ProfileDTO mapToProfileDTO(ProfileRequest profileRequest) {
+        return modelMapper.map(profileRequest, ProfileDTO.class);
+    }
+
+    /**
+     * Mapper method to map values from profile dto to profile response
+     * 
+     * @param profileDTO
+     * @return profileResponse
+     */
+    private ProfileResponse mapToProfileResponse(ProfileDTO profileDTO) {
+        return modelMapper.map(profileDTO, ProfileResponse.class);
+    }
 }
--- src/main/java/com/example/product_api/repositories/ExpenseRepository.java
+++ src/main/java/com/example/product_api/repositories/ExpenseRepository.java
@@ -1,5 +1,23 @@
 package com.example.product_api.repositories;
 
-public class ExpenseRepository {
-    
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.product_api.entity.ExpenseEntity;
+
+public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
+    /**
+     * It will find the single expense from database
+     * 
+     * @param expenseId
+     * @return Optional
+     */
+
+    Optional<ExpenseEntity> findByExpenseId(String expenseId);
+
+    List<ExpenseEntity> findByOwnerId(Long ownerId);
+
+    Optional<ExpenseEntity> findByIdAndOwnerId(Long id, Long ownerId);
 }
